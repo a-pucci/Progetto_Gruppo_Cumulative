fa7e8824cc65e99eb9233efdeb560fe48501e74f
Lorenzo Calosi <lorenzo.calosi1@gmail.com>
2017-04-04 22:18:40 +0200

    Added Player Control Script, Basic Health Functions

---
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Player Scripts/HealthManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+
+	[SerializeField] private int m_StartingHealth = 3;
+	[SerializeField] private int m_CurrentHealth = 0;
+
+	private SpriteRenderer m_SpriteRenderer;
+
+	void Start()
+	{
+		m_SpriteRenderer = GetComponent<SpriteRenderer>();
+		m_CurrentHealth = m_StartingHealth;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag ("Enemy")) {
+			m_CurrentHealth -= 1;
+			if (m_CurrentHealth <= 0) {
+				Time.timeScale = 0;
+				m_SpriteRenderer.enabled = false;
+			}
+		}
+	}
+}
---
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Player Scripts/PlayerUserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+	[RequireComponent(typeof (PlayerMovement))]
+	public class PlayerUserController : MonoBehaviour
+	{
+		private PlayerMovement m_Character;
+		private bool m_Jump;
+
+
+		private void Awake()
+		{
+			m_Character = GetComponent<PlayerMovement>();
+		}
+
+
+		private void Update()
+		{
+			if (!m_Jump) {
+				// Read the jump input in Update so button presses aren't missed.
+				m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			}
+		}
+
+
+		private void FixedUpdate() {
+			// Read the inputs.
+			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			// Pass all parameters to the character control script.
+			m_Character.Move(h, m_Jump);
+			m_Jump = false;
+		}
+	}
+}
