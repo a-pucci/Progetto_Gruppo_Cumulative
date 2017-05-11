26217dede9ce6ee057354689329fbb0c1922ded2
Alessandro Pucci <a_pucci@hotmail.it>
2017-05-11 16:53:37 +0200

    Created StageObject script

---
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Objects Scripts/StageObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StageObject : MonoBehaviour 
+{
+	protected int ID;
+	protected bool canSwapStage;
+
+	public virtual int getID() { return ID;}
+	public virtual bool canSwap() { return canSwapStage;}
+}
