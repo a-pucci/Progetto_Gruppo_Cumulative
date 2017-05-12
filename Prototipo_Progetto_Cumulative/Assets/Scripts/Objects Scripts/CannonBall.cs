24cd4d7d09d1d2d1ee780a77350c0a02b13d0a7d
Alessandro Pucci <a_pucci@hotmail.it>
2017-05-12 21:44:06 +0200

    Created CannonBall and his Script

---
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Objects Scripts/CannonBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : StageObject 
+{
+
+	void OnTriggerEnter2D(Collider2D other)
+	{	
+		if(other.gameObject.GetComponent<StageObject> () != null && 
+			(other.gameObject.GetComponent<StageObject> ().ID == (int)IDList.ID.Box ||
+			other.gameObject.GetComponent<StageObject> ().ID == (int)IDList.ID.Gate))
+			{
+				GameObject.Destroy (other.gameObject);
+			}
+	}
+}
--- Prototipo_Progetto_Cumulative/Assets/Scripts/Objects Scripts/IDList.cs
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Objects Scripts/IDList.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public static class IDList
 {   
-	//                0     1     2    3      4     5     6      7
-	public enum ID {Dummy, Mask, Key, Gear, Torch, Box, Button, Gate};
+	//                0     1     2    3      4     5     6      7       8        9
+	public enum ID {Dummy, Mask, Key, Gear, Torch, Box, Button, Gate, Cannon, CannonBall};
 }
