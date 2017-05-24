d17d6f8106fa496b845818e25889234759d90334
Alessandro Pucci <a_pucci@hotmail.it>
2017-05-24 21:22:30 +0200

    Fixed some bugs

--- Prototipo_Progetto_Cumulative/Assets/Scripts/Objects Scripts/Gate.cs
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Objects Scripts/Gate.cs
@@ -6,15 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class Gate : StageObject 
 {
+	private Collider2D _collider;
+
+	void Start()
+	{
+		_collider = this.gameObject.transform.FindChild ("Collider").gameObject.GetComponent <Collider2D> ();
+	}
+
 	public void openGate()
 	{
 		SpriteRenderer color = this.gameObject.GetComponent<SpriteRenderer> ();
 		color.color = new Color (255, 255, 255);
+		_collider.enabled = false;
 	}
 
 	public void closeGate()
 	{
 		SpriteRenderer color = this.gameObject.GetComponent<SpriteRenderer> ();
 		color.color = new Color (0, 0, 0);
+		_collider.enabled = true;
 	}
 }
