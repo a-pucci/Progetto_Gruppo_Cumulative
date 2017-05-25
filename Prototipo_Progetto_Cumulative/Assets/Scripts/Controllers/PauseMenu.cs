d1c84c8e37b79378b1494cc031831edcdba0482c
Lorenzo Calosi <lorenzo.calosi1@gmail.com>
2017-05-25 11:16:14 +0200

    Added Pause Menu Prefab

---
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Controllers/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public KeyCode pauseButton;
+	[SerializeField] private bool paused = false;
+	public string menuScene;
+	private Canvas thisCanvas;
+
+	public void ResumeGame()
+	{
+		thisCanvas.enabled = false;
+		paused = false;
+		Time.timeScale = 1;
+	}
+
+	public void StopGame()
+	{
+		thisCanvas.enabled = true;
+		paused = true;
+		Time.timeScale = 0;
+	}
+
+	public void ReturnToMenu()
+	{
+		Time.timeScale = 1;
+		SceneManager.LoadScene (menuScene);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log ("Quitting Game");
+		Application.Quit ();
+	}
+
+	void Start() {
+		thisCanvas = this.gameObject.GetComponent<Canvas> ();
+		thisCanvas.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (pauseButton)) {
+			if (paused) {
+				ResumeGame ();
+			} else {
+				StopGame ();
+			}
+		}
+	}
+}
