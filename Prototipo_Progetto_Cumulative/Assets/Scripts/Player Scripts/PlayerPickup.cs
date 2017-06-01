88d7d9236b9be1c044eebfbb96359a9284ebe4bb
Alessandro Pucci <a_pucci@hotmail.it>
2017-06-01 12:57:01 +0200

    WIP Enemy key insert

--- Prototipo_Progetto_Cumulative/Assets/Scripts/Player Scripts/PlayerPickup.cs
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Player Scripts/PlayerPickup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEditor;
 
 public class PlayerPickup : MonoBehaviour 
 {
