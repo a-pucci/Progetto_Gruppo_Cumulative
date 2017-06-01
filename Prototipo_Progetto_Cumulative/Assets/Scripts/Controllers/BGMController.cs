0b83a4ef208161e77f3cc6a20e1596ea3f16e5fc
Lorenzo Calosi <lorenzo.calosi1@gmail.com>
2017-06-01 11:20:57 +0200

    Added Sound Mixers and Controllers

---
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Controllers/BGMController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class BGMController : MonoBehaviour
+{
+	private	AudioSource source;
+	private StageSwapController stageSwap;
+	public AudioMixerGroup bgmMixerA;
+	public AudioMixerGroup bgmMixerB;
+
+	void Start()
+	{
+		stageSwap = GameObject.FindGameObjectWithTag ("StageSwap").GetComponent<StageSwapController> ();
+		source = this.GetComponent<AudioSource> ();
+	}
+
+	void Update ()
+	{
+		if (stageSwap.toggle) {
+			source.outputAudioMixerGroup = bgmMixerB;
+		} else {
+			source.outputAudioMixerGroup = bgmMixerA;
+		}
+	}
+}
---
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Controllers/SFXController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SFXController : MonoBehaviour
+{
+	private AudioSource source;
+
+	void Start() {
+		source = this.GetComponent<AudioSource> ();
+	}
+
+	public void PlaySFX(AudioClip sfx) {
+		source.clip = sfx;
+		source.Play ();
+		source.clip = null;
+	}
+}
