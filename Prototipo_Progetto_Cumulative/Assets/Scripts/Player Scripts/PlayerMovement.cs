3c965bb486844898fdff2e1d4aed8d1d379b22ce
Alessandro Pucci <a_pucci@hotmail.it>
2017-06-14 19:19:27 +0200

    Fixed levels 3-4

--- Prototipo_Progetto_Cumulative/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Prototipo_Progetto_Cumulative/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] private LayerMask _whatIsGround;                  // A mask determining what is ground to the character
 
 	private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
-	const float k_GroundedRadius = .1f; // Radius of the overlap circle to determine if grounded
+	const float k_GroundedRadius = .25f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
 	private Rigidbody2D m_Rigidbody2D;
 
